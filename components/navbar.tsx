1e4486993106853eb2d2874ae64b015e2ff5ca9c
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2024-12-19 18:06:21 +0530

    fix: navigation bar z-index

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import { userData } from "@/lib/data";
 
 export default function Navbar() {
 	return (
-		<header className="bg-dark-accent sticky top-0 backdrop-filter backdrop-blur-lg bg-opacity-30 z-10 border-b border-neutral-800">
+		<header className="bg-dark-accent sticky top-0 backdrop-filter backdrop-blur-lg bg-opacity-30 z-50 border-b border-neutral-800">
 			<nav className="flex items-center max-w-6xl h-16 justify-between mx-auto px-5 md:px-8 lg:px-0">
 				<UserEmail mail={userData.email} name={userData.name} />
 				<Breadcrumbs />
