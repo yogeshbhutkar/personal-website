1793230920a1a8ff16eb0bd8a4494d0b7219d233
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2024-12-19 18:13:16 +0530

    fix: adjust spacing and margins in LatestBlogs and Timeline components

--- components/latest-blogs.tsx
+++ components/latest-blogs.tsx
@@ -4,11 +4,11 @@ import { OGPreview } from "./og-preview";
 
 export default function LatestBlogs() {
 	return (
-		<section className="mt-10 mb-20">
+		<section className="mt-10 mb-20 mx-4 md:mx-0">
 			<h2 className="text-4xl md:text-5xl font-[family-name:var(--font-otterco-display)]">
 				Latest Blogs
 			</h2>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 mt-7">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-6 mt-7">
 				{userData.latestBlogs.map((blog, index) => (
 					<OGPreview key={index} url={blog} />
 				))}
