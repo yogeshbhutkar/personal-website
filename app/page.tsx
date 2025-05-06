992ac9628f74c1e5aa2714132fc8d9e5273da8c2
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-05-06 18:01:26 +0530

    feat: add GSAP animations :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
+"use client";
+
 import UserProfile from "@/components/user-profile";
 import Navbar from "../components/navbar";
 import ContentProvider from "@/components/content-provider";
 import Experience from "@/components/experience";
 import Footer from "@/components/footer";
 import LatestBlogs from "@/components/latest-blogs";
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { useGSAP } from '@gsap/react';
+
+if (typeof window !== 'undefined') {
+	gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+}
 
 export default function Home() {
+
+	useGSAP(
+		() => {
+		  ScrollSmoother.create({
+			smooth: 1,
+			effects: true,
+			content: '.animate-smooth',
+			normalizeScroll: true,
+		  });
+		},
+	  );
+
 	return (
-		<>
+		<main className="animate-smooth">
 			<Navbar />
 			<ContentProvider>
 				<UserProfile />
@@ -15,6 +37,6 @@ export default function Home() {
 				<LatestBlogs />
 			</ContentProvider>
 			<Footer />
-		</>
+		</main>
 	);
 }
--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -1,27 +1,83 @@
 import { userData } from "@/lib/data";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 import GitHubRedirect from "./github-redirect";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function UserProfile() {
+	const profileSection = useRef<HTMLDivElement>(null);
+
+	useGSAP( () => {
+		gsap.to(".hero-asset-animate", {
+			y: 50,
+			duration: 5,
+			repeat: -1,
+			yoyo: true,
+			ease: "sine.inOut",
+		});
+	}, { scope: profileSection } )
+
 	return (
-		<main className="flex flex-col items-center justify-center text-center h-[90vh]">
-			<div className="rounded-full overflow-hidden w-44 h-44 -mt-32 md:-mt-10">
+		<section
+			className="relative flex flex-col items-center justify-center text-center h-[90vh]"
+			ref={profileSection}
+		>
+			<div className="absolute right-0 top-0 h-full hidden md:flex flex-col items-center justify-center space-y-32">
 				<Image
-					src={"/images/user-profile.jpg"}
-					alt="User profile"
-					width={200}
-					height={200}
-					className="object-cover w-full h-full transition-transform duration-300 ease-in-out scale-110 hover:scale-125"
+					src={"/icons/rocket.svg"}
+					className="hero-asset-animate"
+					alt="data-icon"
+					height={250}
+					width={250}
+					draggable={false}
+				/>
+				<Image 
+					src={"/icons/diamond.svg"}
+					className="hero-asset-animate"
+					alt="data-icon"
+					height={300}
+					width={300}
+					draggable={false}
 				/>
 			</div>
-			<p className="mt-3 text-neutral-400 text-lg">
-				{userData.introLine}
-			</p>
-			<h1 className="font-[family-name:var(--font-otterco-display)] font-normal text-5xl md:text-7xl max-w-2xl mt-6 bg-gradient-to-r from-neutral-400 via-neutral-500 to-neutral-800 text-transparent bg-clip-text bg-300% animate-gradient">
-				{userData.about}
-			</h1>
-			<GitHubRedirect />
-		</main>
+			<div data-speed="clamp(1.25)" className="flex flex-col items-center justify-center">
+				<div className="rounded-full overflow-hidden w-44 h-44  md:-mt-10">
+					<Image
+						src={"/images/user-profile.jpg"}
+						alt="User profile"
+						width={200}
+						height={200}
+						className="object-cover w-full h-full transition-transform duration-300 ease-in-out scale-110 hover:scale-125 select-none"
+						draggable={false}
+					/>
+				</div>
+				<p className="mt-3 text-neutral-400 text-lg">
+					{userData.introLine}
+				</p>
+				<h1 className="font-[family-name:var(--font-otterco-display)] font-normal text-5xl md:text-7xl max-w-2xl mt-6 bg-gradient-to-r from-neutral-400 via-neutral-500 to-neutral-800 text-transparent bg-clip-text bg-300% animate-gradient">
+					{userData.about}
+				</h1>
+				<GitHubRedirect />
+			</div>
+			<div className="absolute left-0 top-0 h-full hidden md:flex flex-col items-center justify-center space-y-8">
+				<Image
+					className="hero-asset-animate"
+					src={"/icons/ufo.svg"}
+					alt="data-icon"
+					height={250}
+					width={250}
+					draggable={false}
+				/>
+				<Image
+					src={"/icons/text.svg"}
+					className="hero-asset-animate"
+					alt="data-icon"
+					height={250}
+					width={250}
+					draggable={false}
+				/>
+			</div>
+		</section>
 	);
 }
